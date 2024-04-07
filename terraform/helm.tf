efb2663bfdfe831ff4e77c5561b5ed75dd5e6604
bolasto <mobolatitoo@gmail.com>
2024-04-07 02:55:20 +0100

    first commit

---
+++ terraform/helm.tf
@@ -0,0 +1,43 @@
+####################Create ArgoCD Namespace####################
+####################PLEASE MAKE SURE YOUR CLUSTER IS CREATED BEFORE DEPLOYING THIS FILE####################
+
+resource "helm_release" "argocd" {
+  count = local.create_workloads == true ? 1 : 0 
+
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "6.7.10"
+
+  #   namespace = kubernetes_namespace.argocd[0].metadata[0].name
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [
+    templatefile("values/argo-value.yaml", { env = var.env })
+  ]
+}
+
+####################Create ArgoCD Apps####################
+
+resource "helm_release" "argocd_apps" {
+  count = local.create_workloads == true ? 1 : 0
+
+  name       = "argo-cd-apps"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  version    = "2.0.0"
+
+  #   namespace = kubernetes_namespace.argocd[0].metadata[0].name
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [
+    templatefile("values/apps-value.yaml", { env = var.env })
+  ]
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
+####################Create ArgoCD Apps####################
---
+++ terraform/variable.tf
@@ -0,0 +1,35 @@
+# Description: This file contains all the variables used in the terraform code####################
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "env" {
+  description = "Environment variable abbreviation"
+  type        = string
+  default     = "prod"
+
+  validation { 
+    condition     = contains(["staging", "prod"], var.env) #
+    error_message = "Invalid argument \"env\", please choose one of: (\"staging\",\"prod\")."
+  }
+}
+
+### VPC variables
+variable "single_nat_gateway" {
+  type        = bool
+  default     = true
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks. Consider setting to false for HA in production env."
+}
+
+
+### EKS variables
+variable "eks_default_instance_type" {
+  type        = string
+  default     = "t2.medium" # t2.medium is the default instance type for EKS
+  description = "Default instance type for EKS cluster"
+}
+
+############################################################################################################
